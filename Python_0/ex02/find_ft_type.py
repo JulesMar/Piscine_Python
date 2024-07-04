133f0d296546c844dd20712b3a5f72ae4dd2d4ce
Jules Martin <julemart@1F3.42angouleme.fr>
2024-07-04 17:56:58 +0200

    ex02 clean

--- Python_0/ex02/find_ft_type.py
+++ Python_0/ex02/find_ft_type.py
@@ -1,3 +1,8 @@
 def all_thing_is_obj(object: any) -> int:
-    print(type(object))
+    if isinstance(object, (list, tuple, set, dict)):
+        print(str(type(object))[8:-2].capitalize(), ":", type(object))
+    elif type(object) is str:
+        print(object, "is in the kitchen :", type(object))
+    else:
+        print("Type not found")
     return (42)
