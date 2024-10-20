2e64c38584608f0450da1ce7e3f30452c3b1775e
julesmar <julesmartin99@hotmail.com>
2024-10-20 19:19:11 +0200

    qw

--- Python_3/ex01/S1E7.py
+++ Python_3/ex01/S1E7.py
@@ -5,6 +5,9 @@ class Baratheon(Character):
     def __init__(self, first_name: str, is_alive: bool = True):
         """Your docstring for Constructor"""
         super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
 
     def die(self):
         """Your docstring for Method"""
@@ -13,12 +16,19 @@ class Baratheon(Character):
         else:
             print(f"{self.first_name} is already dead.")
 
+    def __str__(self):
+        return f'coucou'
+
 
 class Lannister(Character):
     """Your docstring for Class"""
     def __init__(self, first_name: str, is_alive: bool = True):
         """Your docstring for Constructor"""
         super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
 
     def die(self):
         """Your docstring for Method"""
